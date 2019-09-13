2b091101595e80cc1cf47e6cf09e3ed4c141981e
Caleb Redd <caleb.redd@yahoo.com>
2019-09-12 19:21:27 -0700

    Functions associated with multiple buttons, can keep track of current inning, atBat team, as well as how many are on base. Homerun button. Tag button active. Lets play ball

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,152 @@
 import React from "react";
 import "../Dashboard.scss";
+import Display from "./Display";
 
-export default function Dashboard() {
+export default function Dashboard(props) {
+  const {
+    fouls,
+    setFouls,
+    strikes,
+    setStrikes,
+    balls,
+    setBalls,
+    outs,
+    setOuts,
+    awayRuns,
+    setAwayRuns,
+    homeRuns,
+    setHomeRuns,
+    hits,
+    setHits,
+    inning,
+    setInning,
+    top,
+    setTop
+  } = props;
+  const newGame = () => {
+    setInning(1);
+    setTop(true);
+    newBatter();
+    setAwayRuns(0);
+    setHomeRuns(0);
+    document.getElementsByClassName("awayBatting")[0].classList.remove("atBat");
+    document.getElementsByClassName("homeBatting")[0].classList.add("atBat");
+  };
+  const switchAtBatTeam = () => {
+    document.getElementsByClassName("awayBatting")[0].classList.toggle("atBat");
+    document.getElementsByClassName("homeBatting")[0].classList.toggle("atBat");
+    setOuts(0);
+    newBatter();
+    setHits(0);
+    setTop(!top);
+    if (!top) {
+      setInning(inning + 1);
+      if (inning > 8) {
+        newGame();
+      }
+    }
+  };
+  const newBatter = () => {
+    setStrikes(0);
+    setFouls(0);
+    setBalls(0);
+  };
+  const out = () => {
+    if (outs < 2) {
+      setOuts(outs + 1);
+      newBatter();
+    } else {
+      switchAtBatTeam();
+    }
+  };
+  const strike = () => {
+    if (strikes < 2) {
+      setStrikes(strikes + 1);
+    } else {
+      out();
+    }
+  };
+  const foul = () => {
+    if (strikes < 2) setStrikes(strikes + 1);
+  };
+  const hit = () => {
+    if (hits === 3) {
+      if (top) {
+        setHomeRuns(homeRuns + 1);
+      } else {
+        setAwayRuns(awayRuns + 1);
+      }
+      newBatter();
+    } else {
+      setHits(hits + 1);
+      newBatter();
+    }
+  };
+  const ball = () => {
+    if (balls < 3) {
+      setBalls(balls + 1);
+    } else {
+      hit();
+    }
+  };
+  const beaned = () => {
+    hit();
+  };
+  const tag = () => {
+    if (hits > 0) {
+      setHits(hits - 1);
+    }
+    out();
+  };
+  const homerun = () => {
+    if (top) {
+      setHomeRuns(homeRuns + hits + 1);
+    } else {
+      setAwayRuns(awayRuns + hits + 1);
+    }
+    setHits(0);
+    newBatter();
+  };
+
   return (
-    <div className="Dashboard">
-      <h2>Umpire Controls</h2>
-      <div className="buttons">
-        <button className="strike">Strike</button>
-        <button className="foul">Foul</button>
-        <button className="ball">Ball</button>
+    <div>
+      <Display
+        balls={balls}
+        strikes={strikes}
+        fouls={fouls}
+        awayRuns={awayRuns}
+        homeRuns={homeRuns}
+        outs={outs}
+        hits={hits}
+      />
+      <div className="Dashboard">
+        <h2>Umpire Controls</h2>
+        <div className="buttons">
+          <button className="hit" onClick={hit}>
+            Hit
+          </button>
+          <button className="foul" onClick={foul}>
+            Foul
+          </button>
+          <button className="strike" onClick={strike}>
+            Strike
+          </button>
+          <button className="ball" onClick={ball}>
+            Ball
+          </button>
+          <button className="beaned" onClick={beaned}>
+            Beaned
+          </button>
+          <button className="tag" onClick={tag}>
+            Tag
+          </button>
+          <button className="homerun" onClick={homerun}>
+            Homerun
+          </button>
+          <button className="reset" onClick={newGame}>
+            Reset
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/Display.js
+++ src/components/Display.js
@@ -2,14 +2,19 @@ import React from "react";
 import "../Display.scss";
 
 export default function Display(props) {
-  const { strikes, balls, runsHome, runsAway, outs } = props;
+  const { strikes, balls, outs, awayRuns, homeRuns, hits } = props;
   return (
     <div className="display">
       <div className="score">
         <div className="homeTeam">
+          <img
+            className="homeBatting atBat"
+            src="https://images.unsplash.com/photo-1554591203-d6433caa8495?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+            alt="At Bat"
+          />
           <h2>Home</h2>
           <h3>Runs</h3>
-          <p>{runsHome}</p>
+          <p>{homeRuns}</p>
         </div>
         <div className="batter">
           <div>
@@ -24,11 +29,20 @@ export default function Display(props) {
             <h3 className="outs">Outs</h3>
             <p>{outs}</p>
           </div>
+          <div>
+            <h3 className="bases">On Base</h3>
+            <p>{hits}</p>
+          </div>
         </div>
         <div className="awayTeam">
+          <img
+            className="awayBatting"
+            src="https://images.unsplash.com/photo-1554591203-d6433caa8495?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+            alt="At Bat"
+          />
           <h2>Away</h2>
           <h3>Runs</h3>
-          <p>{runsAway}</p>
+          <p>{awayRuns}</p>
         </div>
       </div>
     </div>
